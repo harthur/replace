b9c6873c3dc91c8241a665c184ae2b0014b8898e
Heather Arthur <fayearthur@gmail.com>
2013-02-19 16:25:03 -0800

    share some option specs between search and replace

--- bin/replace.js
+++ bin/replace.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 
-var path = require("path"),
-    nomnom = require("nomnom"),
-    replace = require("../replace");
+var nomnom = require("nomnom"),
+    replace = require("../replace"),
+    sharedOptions = require("./shared-options");
 
-var options = nomnom.options({
-    regex: {
-        position: 0,
-        help: "JavaScript regex for searching file e.g. '\\d+'",
-        required: true
-    },
+/* Additional options that apply to `replace`, but not `search` */
+var addlOptions = {
     replacement: {
         position: 1,
         help: "Replacement string for matches",
@@ -17,84 +13,42 @@ var options = nomnom.options({
     },
     paths: {
         position: 2,
-        help: "File or directory to search (default is '.')",
+        help: "File or directory to search (default is '*')",
         list: true,
-        default: ["."]
-    },
-    recursive: {
-        abbr: 'r',
-        flag: true,
-        help: "Recursively search and replace in directories"
-    },
-    preview: {
-        abbr: 'p',
-        flag: true,
-        help: "Preview the replacements, but don't modify files"
-    },
-    ignoreCase: {
-        abbr: 'i',
-        full: 'ignore-case',
-        flag: true,
-        help: "Ignore case when matching"
-    },
-    multiline: {
-        abbr: 'm',
-        flag: true,
-        help: "Match line by line, default is true",
-        default: true
+        default: ["*"]
     },
     funcFile: {
         abbr: 'f',
         full: 'function-file',
         metavar: 'PATH',
-        help: 'file containing JS replacement function'
-    },
-    include: {
-        string: '--include=PATHS',
-        help: "Only search in these files, e.g. '*.js,*.foo'"
-    },
-    exclude: {
-        string: '--exclude=PATHS',
-        help: "Don't search in these files, e.g. '*.min.js'"
-    },
-    excludeList: {
-        full: 'exclude-list',
-        metavar: 'FILE',
-        help: "File containing a new-line separated list of files to ignore",
-        default: path.join(__dirname, "..", "defaultignore"),
+        help: 'file containing JS replacement function',
         hidden: true
     },
     maxLines: {
         string: '-n NUMLINES',
         help: 'limit the number of lines to preview'
     },
-    count: {
-        abbr: 'c',
-        flag: true,
-        help: 'display count of occurances in each file'
-    },
-    quiet: {
-        abbr: 'q',
-        flag: true,
-        help: "Just print the names of the files matches occured in (faster)"
-    },
     silent: {
         abbr: 's',
         flag: true,
         help: "Don't print out anything"
     },
-    color: {
-        metavar: 'COLOR',
-        help: "highlight color, e.g. 'green' or 'blue'",
-        choices: ['red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'bold', 'italic'],
-        default: 'cyan'
-    },
-    async: {
-        abbr: 'a',
+    preview: {
+        abbr: 'p',
         flag: true,
-        help: "asynchronously read/write files in directory (faster)"
+        help: "Preview the replacements, but don't modify files"
     }
-  })
+}
+
+var opts = {};
+for (var opt in sharedOptions) {
+    opts[opt] = sharedOptions[opt];
+}
+for (var opt in addlOptions) {
+    opts[opt] = addlOptions[opt];
+}
+
+var options = nomnom.options(opts)
   .script("replace")
   .parse();
 
--- bin/search.js
+++ bin/search.js
@@ -1,78 +1,10 @@
 #!/usr/bin/env node
 
-var path = require("path"),
-    nomnom = require("nomnom"),
-    replace = require("../replace");
+var nomnom = require("nomnom"),
+    replace = require("../replace"),
+    sharedOptions = require("./shared-options");
 
-var options = nomnom.options({
-    regex: {
-        position: 0,
-        help: "JavaScript regex for searching file e.g. '\\d+'",
-        required: true
-    },
-    paths: {
-        position: 1,
-        help: "File or directory to search (default is '.')",
-        list: true,
-        default: ["."]
-    },
-    recursive: {
-        abbr: 'r',
-        flag: true,
-        help: "Recursively search directories"
-    },
-    ignoreCase: {
-        abbr: 'i',
-        flag: true,
-        help: "Ignore case when searching"
-    },
-    multiline: {
-        abbr: 'm',
-        flag: true,
-        help: "Match line by line, default is true",
-        default: true
-    },
-    include: {
-        string: '--include=PATHS',
-        help: "Only search in these files, e.g. '*.js,*.foo'"
-    },
-    exclude: {
-        string: '--exclude=PATHS',
-        help: "Don't search in these files, e.g. '*.min.js'"
-    },
-    excludeList: {
-        full: 'exclude-list',
-        metavar: 'FILE',
-        help: "File containing a new-line separated list of files to ignore",
-        default: path.join(__dirname, "..", "defaultignore"),
-        hidden: true
-    },
-    maxLines: {
-        string: '-n NUMLINES',
-        help: 'limit the number of lines to preview'
-    },
-    count: {
-        abbr: 'c',
-        flag: true,
-        help: 'display count of occurances in each file'
-    },
-    quiet: {
-        abbr: 'q',
-        flag: true,
-        help: "Just print the names of the files matches occured in (faster)"
-    },
-    color: {
-        metavar: 'COLOR',
-        help: "highlight color, e.g. 'green' or 'blue'",
-        choices: ['red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'bold', 'italic'],
-        default: 'cyan'
-    },
-    async: {
-        abbr: 'a',
-        flag: true,
-        help: "asynchronously read/write files in directory (faster)"
-    }
-  })
+var options = nomnom.options(sharedOptions)
   .script("search")
   .parse();
 
